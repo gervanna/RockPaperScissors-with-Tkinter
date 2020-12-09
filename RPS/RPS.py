a741217353134208ab71c6fdcca6513fdf1e5f03
Gervanna Stephens <stephenslavern@gmail.com>
2020-12-08 18:03:27 -0700

    Update RPS.py

--- RPS/RPS.py
+++ RPS/RPS.py
@@ -1,30 +1,56 @@
 import random
-from tkinter import Tk, Button, Label, Text, PhotoImage, TOP, BOTTOM
+from tkinter import Tk, Button, Label, PhotoImage, TOP, BOTTOM, Frame
 
 # base gui initialization
 root = Tk()
 root.wm_title = "Rock Paper Scissors" #app title
 root.geometry("600x600") #window size
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+startPage=Frame(root)
+startPage.grid(row=0, column=0, sticky="nsew")
+
+gamePage=Frame(root)
+gamePage.grid(row=0, column=0, sticky="nsew")
+startPage.tkraise()
 
 # grabbing our images
 rock = PhotoImage(file="/Users/gervannastephens/Documents/GitHub/RockPaperScissors-with-Tkinter/RPS/rock-100x120.png")
-
 paper = PhotoImage(file="/Users/gervannastephens/Documents/GitHub/RockPaperScissors-with-Tkinter/RPS/paper-100x120.png")
-
 scissors = PhotoImage(file="/Users/gervannastephens/Documents/GitHub/RockPaperScissors-with-Tkinter/RPS/scissors-100x120.png")
 
-
 game_choices = [rock, paper, scissors]
+style_button = ('Verdana', 14, 'bold')
 
-
-welcome = Label(root, padx=10, pady=10, fg='blue', font='Verdana 24 bold', text= "Welcome to Rock Paper Scissors.")
+welcome = Label(startPage, padx=10, pady=10, fg='blue', font='Verdana 24 bold', text= "Welcome to Rock Paper Scissors.")
 welcome.pack()
-rules = '''
-The outcome of the game is determined by 3 simple rules:
-Rock wins against scissors.
-Scissors win against paper.
-Paper wins against rock.
-'''
+rules = '''The outcome of the game is determined by 3 simple rules:
+\n1 Rock wins against scissors.
+\n2. Scissors win against paper.
+\n3. Paper wins against rock.'''
+info = Label(startPage, font='Verdana 14', bg='#B0C4DE', text=rules)
+info.pack()
+
+winLimit = 0
+
+def singleStart():
+    global winLimit
+    winLimit = 1
+    gamePage.tkraise()
+
+def multigameStreak():
+    global winLimit
+    winLimit = 10
+    gamePage.tkraise()
+
+
+single=Button(startPage, font=style_button, text="Single", command=singleStart)
+single.pack()
+
+streak=Button(startPage, font=style_button, text="Streak", command=multigameStreak)
+streak.pack()
+
 
 def pickedRock():
     player_choice= 0
@@ -38,28 +64,28 @@ def pickedScissors():
     player_choice = 2
     runGame(player_choice)
 
-rockButton = Button(root, font='Verdana 14 bold', text="rock", command=pickedRock)
+rockButton = Button(gamePage, font=style_button, padx=2, pady=2, text="rock", command=pickedRock)
 rockButton.pack()
 
-paperButton = Button(root, font='Verdana 14 bold', text="paper", command=pickedPaper)
+paperButton = Button(gamePage, font=style_button, text="paper", command=pickedPaper)
 paperButton.pack()
 
-scissorsButton = Button(root, font='Verdana 14 bold', text="scissors", command=pickedScissors)
+scissorsButton = Button(gamePage, font=style_button, text="scissors", command=pickedScissors)
 scissorsButton.pack()
 
-player_choice_label = Label(root)
+player_choice_label = Label(gamePage)
 player_choice_label.pack()
 
-computer_choice_label = Label(root)
+computer_choice_label = Label(gamePage)
 computer_choice_label.pack()
 
-statusLabel = Label(root)
+statusLabel = Label(gamePage)
 statusLabel.pack()
 
-player_score_label = Label(root)
+player_score_label = Label(gamePage)
 player_score_label.pack()
 
-computer_score_label = Label(root)
+computer_score_label = Label(gamePage)
 computer_score_label.pack()
 
 player_score = 0
@@ -76,27 +102,31 @@ def resetGame():
     computer_score = 0
     player_score_label.configure(text="")
     computer_score_label.configure(text="")
+    rockButton.configure(state="normal")
+    paperButton.configure(state="normal")
+    scissorsButton.configure(state="normal")
+    resetButton.configure(state="disabled")
+    startPage.tkraise()
 
-resetButton = Button(root, text="Reset", command=resetGame)
+
+resetButton = Button(gamePage, font=style_button, text="Reset", command=resetGame, state="disabled")
 resetButton.pack()
 
 def exitGame():
     root.destroy()
 
-exitButton = Button(root, text="exit app", command=exitGame)
+exitButton = Button(gamePage, font=style_button, text="Exit App", command=exitGame)
 exitButton.pack()
 
-
 def runGame(player_choice):
-
     global player_score
     global computer_score
 
-    player_choice_label.configure(text="You chose: ", image=game_choices[player_choice], compound=BOTTOM)
+    player_choice_label.configure(text="You chose:\n", image=game_choices[player_choice], compound=BOTTOM)
 
     computer_choice = random.randint(0, 2)
 
-    computer_choice_label.configure(text="The computer chose: ", image=game_choices[computer_choice], compound=BOTTOM)
+    computer_choice_label.configure(text="The computer chose:\n", image=game_choices[computer_choice], compound=BOTTOM)
 
     if player_choice == computer_choice:
         statusLabel.configure(text="\nIt's a draw.")
@@ -120,14 +150,17 @@ def runGame(player_choice):
     player_score_label.configure(text=f"Your score: {player_score}")
     computer_score_label.configure(text=f"Computer's score: {computer_score}")
 
-    if player_score == 10:
+    if player_score == winLimit:
         statusLabel.configure(text="you won the whole thing!")
-        resetGame()
-    elif computer_score == 10:
+        rockButton.configure(state="disabled")
+        paperButton.configure(state="disabled")
+        scissorsButton.configure(state="disabled")
+        resetButton.configure(state="normal")
+    elif computer_score == winLimit:
         statusLabel.configure(text="you lost to a robot.")
-        resetGame()
-
-    
-
+        rockButton.configure(state="disabled")
+        paperButton.configure(state="disabled")
+        scissorsButton.configure(state="disabled")
+        resetButton.configure(state="normal")
 
 root.mainloop()
